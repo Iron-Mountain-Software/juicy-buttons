c5faa3d553a8d3f626c4d668a35debb914fa7e2f
Chris Monteferrante <chrismonteferrante@Chriss-MacBook-Pro-2.local>
2022-11-08 15:20:09 -0600

    SFX

---
+++ Runtime/JuicyButtonSFX.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+[RequireComponent(typeof(JuicyButton))]
+[RequireComponent(typeof(AudioSource))]
+public class JuicyButtonSFX : MonoBehaviour
+{
+    [Header("SFX")]
+    [SerializeField] private AudioClip buttonDownSound;
+    [SerializeField] private AudioClip buttonUpSound;
+
+    [Header("Cache")]
+    private JuicyButton _button;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _button = GetComponent<JuicyButton>();
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = null;
+        _audioSource.playOnAwake = false;
+    }
+
+    private void OnEnable()
+    {
+        _button.OnPointerDownEvent += OnPointerDown;
+        _button.OnPointerClickEvent += OnPointerClick;
+    }
+
+    private void OnDisable()
+    {
+        _button.OnPointerDownEvent -= OnPointerDown;
+        _button.OnPointerClickEvent -= OnPointerClick;
+    }
+
+    private void OnPointerDown()
+    {
+        if (!_button.Interactable) return;
+        _audioSource.PlayOneShot(buttonDownSound);
+    }
+
+    private void OnPointerClick()
+    {
+        if (!_button.Interactable) return;
+        _audioSource.PlayOneShot(buttonUpSound);
+    }
+}
